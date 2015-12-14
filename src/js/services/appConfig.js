476b82dff347b8bb9c45f8327f1d291bfb488c06
jvalencia <jvalencia@vaduinc.com>
2015-12-13 22:57:20 -0500

    includes CLIENT_ID

--- src/js/services/appConfig.js
+++ src/js/services/appConfig.js
@@ -5,7 +5,7 @@
 
 app.value('appConfig' ,
     {
-        CLIENT_ID : '',
+        CLIENT_ID : '1048872932033-l0jitd54odppd6c896djjq2e1lrer8qg.apps.googleusercontent.com',
         SCOPES : ['https://www.googleapis.com/auth/drive.readonly'],
         ZOMBIE_URL : 'http://ancient-anchorage-9224.herokuapp.com/zombify?q=',
         MAX_RESULTS : 10
